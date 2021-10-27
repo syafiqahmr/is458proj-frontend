e3062b345b4f6bea9179f12941d9a323a5faf3b3
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2021-10-27 20:19:44 +0800

    Add charts

--- scisvan/src/Components/Home.js
+++ scisvan/src/Components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Component } from 'react';
-import { Container, Table, Row } from 'react-bootstrap';
+import { Container, Table, Row, Col } from 'react-bootstrap';
 import NavigationBar from "./NavigationBar";
+import { Line, Bar } from 'react-chartjs-2';
+import { transparentize, CHART_COLORS, OPTIONS_LINE, OPTIONS_STACKED_BAR } from './Utils'
 
 class Home extends Component {
   constructor(props) {
@@ -9,9 +11,67 @@ class Home extends Component {
     this.state = {
       parcelId: "",
       data: [],
+      labels: [],
+      noOfParcels: [],
+      noOfOpen: [],
+      noOfClose: [],
+      avgPrice: [],
       api: "https://u2vjogr2z0.execute-api.us-east-1.amazonaws.com/default/GetAnalyticsData ",
-      hideParcelDetails: true
+      chartReady: false
     }
+  }
+
+  prepareChartData = () => {
+    const noOfParcelChartData = {
+      labels: this.state.labels,
+      datasets: [
+        {
+          label: 'No of Parcels',
+          data: this.state.noOfParcels,
+          fill: false,
+          backgroundColor: CHART_COLORS.blue,
+          borderColor: transparentize(CHART_COLORS.blue, 0.5),
+        },
+      ],
+    };
+
+    const avgPriceChartData = {
+      labels: this.state.labels,
+      datasets: [
+        {
+          label: 'Average Price',
+          data: this.state.avgPrice,
+          fill: false,
+          backgroundColor: CHART_COLORS.blue,
+          borderColor: transparentize(CHART_COLORS.blue, 0.5),
+        },
+      ],
+    };
+
+    const noOfOpenCloseChartData = {
+      labels: this.state.labels,
+      datasets: [
+        {
+          label: 'Open',
+          data: this.state.noOfOpen,
+          borderColor: CHART_COLORS.red,
+          backgroundColor: transparentize(CHART_COLORS.red, 0.5),
+        },
+        {
+          label: 'Close',
+          data: this.state.noOfClose,
+          borderColor: CHART_COLORS.blue,
+          backgroundColor: transparentize(CHART_COLORS.green, 0.5),
+        }
+      ]
+    };
+
+    this.setState({
+      noOfParcelChartData: noOfParcelChartData,
+      noOfOpenCloseChartData: noOfOpenCloseChartData,
+      avgPriceChartData: avgPriceChartData,
+      chartReady: true
+    })
   }
 
   componentDidMount() {
@@ -27,12 +87,23 @@ class Home extends Component {
 
     fetch(this.state.api).then(res => res.json()).then(
       (data) => {
+        // process data
+        var labels = data.map(d => d[1]);
+        var noOfParcels = data.map(d => d[2]);
+        var noOfOpen = data.map(d => d[3]);
+        var noOfClose = data.map(d => d[4]);
+        var avgPrice = data.map(d => d[5]);
+
         this.setState({
-          hideParcelDetails: false,
-          data: data
+          data: data,
+          labels: labels,
+          noOfParcels: noOfParcels,
+          noOfClose: noOfClose,
+          noOfOpen: noOfOpen,
+          avgPrice: avgPrice
         });
-        console.log(data);
-        console.log(this.state.data);
+
+        this.prepareChartData();
       },
       (error) => {
         console.log(error);
@@ -47,31 +118,56 @@ class Home extends Component {
           <Row>
             <NavigationBar />
           </Row>
-          <Container className="my-5 col-sm-4">
-            <h3 className="my-3">Overall Metrics</h3>
-            <h5>Number of parcels delivered</h5>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>No of parcels</th>
-                  <th>Open</th>
-                  <th>Close</th>
-                  <th>Average Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.data.map((d) => (
-                  <tr>
-                    <td>{d[1]}</td>
-                    <td>{d[2]}</td>
-                    <td>{d[3]}</td>
-                    <td>{d[4]}</td>
-                    <td>{Math.round(d[5])}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+          <Container>
+            {
+              this.state.chartReady ?
+                <div>
+                  <Container>
+                    <Row className="my-5">
+                      <Col>
+                        <h5>Number of Parcels Chart</h5>
+                        <Line data={this.state.noOfParcelChartData} options={OPTIONS_LINE} />
+                      </Col>
+                      <Col>
+                        <h5>Average Price Chart</h5>
+                        <Line data={this.state.avgPriceChartData} options={OPTIONS_LINE} />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <h5>Number of Open/Close Parcels Chart</h5>
+                        <Bar data={this.state.noOfOpenCloseChartData} options={OPTIONS_STACKED_BAR} />
+                      </Col>
+                      <Col>
+                        <h5 className="my-3">Full Data</h5>
+                        <Table striped bordered hover>
+                          <thead>
+                            <tr>
+                              <th>Date</th>
+                              <th>No of parcels</th>
+                              <th>Open</th>
+                              <th>Close</th>
+                              <th>Average Price</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {this.state.data.map((d) => (
+                              <tr>
+                                <td>{d[1]}</td>
+                                <td>{d[2]}</td>
+                                <td>{d[3]}</td>
+                                <td>{d[4]}</td>
+                                <td>{Math.round(d[5])}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </Table>
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
+                : null
+            }
           </Container>
         </Container>
       </div>
