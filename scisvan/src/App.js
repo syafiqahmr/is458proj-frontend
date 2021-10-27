07ea8d6f94510fa1b9549e5ee973d1901339a237
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2021-10-27 20:21:29 +0800

    Remove today

--- scisvan/src/App.js
+++ scisvan/src/App.js
@@ -1,7 +1,6 @@
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Components/Home";
-import Today from "./Components/Today";
 import Status from "./Components/Status";
 import Location from "./Components/Location";
 
@@ -11,7 +10,6 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/today" component={Today} />
           <Route exact path="/status" component={Status} />
           <Route exact path="/location" component={Location} />
         </Switch>
